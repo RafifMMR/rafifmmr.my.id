fed72a5372aae0f0881ad2529d58b9b0a13f68e7
RafifMMR <rafifmuhammad2003@gmail.com>
2025-06-18 11:31:26 +0700

    logomark size update

--- src/features/profile/components/profile-cover.tsx
+++ src/features/profile/components/profile-cover.tsx
@@ -5,7 +5,7 @@ import { cn } from "@/lib/utils";
 export function ProfileCover() {
   return (
     <BrandContextMenu>
-      <div
+      {/* <div
         className={cn(
           "aspect-[4/3] border-x border-edge select-none sm:aspect-[3/1]",
           "flex items-center justify-center text-black dark:text-white",
@@ -17,6 +17,20 @@ export function ProfileCover() {
         <RafifMark
           id="js-cover-mark"
           className="max-h-6 w-auto sm:max-h-8 md:max-h-12 lg:max-h-[25%]"
+        />
+      </div> */}
+
+      <div
+        className={cn(
+          "aspect-[3/2] border-x border-edge select-none sm:aspect-[3/1]",
+          "flex items-center justify-center text-black dark:text-white",
+          "screen-line-before screen-line-after before:-top-px after:-bottom-px",
+          "bg-black/0.75 bg-[radial-gradient(var(--pattern-foreground)_1px,transparent_0)] bg-size-[10px_10px] bg-center [--pattern-foreground:var(--color-zinc-950)]/5 dark:bg-white/0.75 dark:[--pattern-foreground:var(--color-white)]/5"
+        )}
+      >
+        <RafifMark
+          id="js-cover-mark"
+          className="h-12 w-auto sm:h-16 md:h-20 lg:h-1/4"
         />
       </div>
     </BrandContextMenu>
