e02a8a7cb4da2e0a43b8f555b9b411482c74bd15
RafifMMR <rafifmuhammad2003@gmail.com>
2025-09-22 09:47:26 +0700

    add certificate of ethereum learner

--- src/features/profile/data/certifications.ts
+++ src/features/profile/data/certifications.ts
@@ -21,4 +21,14 @@ export const CERTIFICATIONS: Certification[] = [
     credentialURL:
       "https://drive.google.com/file/d/1ghGzrDUgXYTHP_-tdAw9EoQpPXPcKCZw/view?usp=drive_link",
   },
+  {
+    title: "Ethereum Learner Certificate",
+    issuer: "HackQuest",
+    issuerLogoURL:
+      "https://res.cloudinary.com/dnuusu1kb/image/upload/v1756093784/hackquest-logo.png",
+    issueDate: "2025-09-22",
+    credentialID: "c2",
+    credentialURL:
+      "https://drive.google.com/file/d/1l6_-JOYz7LFYcpmRG3noJpYfs3gmJH9U/view?usp=drive_link",
+  },
 ];
